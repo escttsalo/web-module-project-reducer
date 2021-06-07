a89bfe3af6dd4531831d4fcad2dab7842046b05a
escttsalo <escttsalo@gmail.com>
2021-06-07 17:17:42 -0600

    display state

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import CalcButton from './components/CalcButton';
 import reducer, { initialState } from './reducers'
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div className="App">
@@ -19,10 +20,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
